467e1aa8e173a5b7147093b224b38731fd9130d1
ARTURO48 <angelarturorc@gmail.com>
2025-08-21 18:11:02 +0000

    CREATED ejemplo

---
+++ Solver/Program.cs
@@ -0,0 +1,13 @@
+﻿   double a = 1;
+
+            if (a == 2)
+            {
+                System.Console.WriteLine("a es igual a 2");
+            }
+            else
+            {
+                System.Console.WriteLine("a no es igual a 2");
+            }
+
+            System.Console.WriteLine("Esto se escribe siempre");
+           
